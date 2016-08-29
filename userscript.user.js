984f42ac57b0af019c6c1a525b13532331ab5fbe
ncla <blindmind6@gmail.com>
2016-08-29 14:05:49 +0300

    User-script for live changes

---
+++ userscript.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         /r/muse stylesheet testing
+// @namespace    http://muse.reddit.com
+// @version      0.1
+// @description  Removes sub-reddits stylesheet and applies local stylesheet
+// @author       ncla
+// @require http://code.jquery.com/jquery-latest.js
+// @match        *://*.reddit.com/r/ncla*
+// @match        *://*.reddit.com/r/musetestsub*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('link[rel="stylesheet"][title="applied_subreddit_stylesheet"]').remove();
+    $('head').append('<link rel="stylesheet" type="text/css" href="https://localhost:4443/build/stylesheet.css" media="all">');
+})();
