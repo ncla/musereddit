d1a64889e829d31f3160da3824e17bd342b2aad6
ncla <blindmind6@gmail.com>
2016-08-31 01:39:27 +0300

    Separating Apicem core CSS and custom CSS in two different files, improve RES drop-down menus

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,17 @@ var replace = require('gulp-replace');
 var cleanCSS = require('gulp-clean-css');
 var gulpif = require('gulp-if');
 var argv = require('yargs').argv;
+var concatCss = require('gulp-concat-css');
 
 gulp.task('build', function() {
-    var stream = gulp.src('stylesheet.css')
+    var stream = gulp.src(['apicem.css', 'custom.css'])
         .pipe(gulpif((argv.production !== true), replace('url(%%body%%)', 'url("https://localhost:4443/images/body.png")')))
         .pipe(gulpif((argv.production !== true), replace('url(%%overlay%%)', 'url("https://localhost:4443/images/overlay.png")')))
-        .pipe(gulpif((argv.production !== true), replace('url(%%header%%)', 'url("https://localhost:4443/images/header.png")')))
+        .pipe(gulpif((argv.production !== true), replace('url(%%header%%)', 'url("https://localhost:4443/images/header.jpg")')))
         .pipe(gulpif((argv.production !== true), replace('url(%%crosshatch%%)', 'url("https://localhost:4443/images/crosshatch.png")')))
         .pipe(gulpif((argv.production !== true), replace('url(%%editbuttons%%)', 'url("https://localhost:4443/images/editbuttons.png")')))
         .pipe(gulpif((argv.production !== true), replace('url(%%spritesheet%%)', 'url("https://localhost:4443/images/spritesheet.png")')))
+        .pipe(concatCss('stylesheet.css'))
         .pipe(cleanCSS({debug: true}))
         .pipe(gulp.dest('build/'));
 
@@ -19,7 +21,7 @@ gulp.task('build', function() {
 });
 
 gulp.task('watch', function() {
-    var watcher = gulp.watch('stylesheet.css', ['build']);
+    var watcher = gulp.watch(['apicem.css', 'custom.css'], ['build']);
 
     watcher.on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
