128a63a3b4c57f852ab48a3f023128dcc8c6708a
ncla <blindmind6@gmail.com>
2016-08-30 19:12:13 +0300

    Restyled header

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var cleanCSS = require('gulp-clean-css');
+var gulpif = require('gulp-if');
+var argv = require('yargs').argv;
 
 gulp.task('build', function() {
     var stream = gulp.src('stylesheet.css')
-        .pipe(replace('url(%%body%%)', 'url("https://localhost:4443/images/body.png")'))
-        .pipe(replace('url(%%overlay%%)', 'url("https://localhost:4443/images/overlay.png")'))
-        .pipe(replace('url(%%header%%)', 'url("https://localhost:4443/images/header.png")'))
-        .pipe(replace('url(%%crosshatch%%)', 'url("https://localhost:4443/images/crosshatch.png")'))
-        .pipe(replace('url(%%editbuttons%%)', 'url("https://localhost:4443/images/editbuttons.png")'))
-        .pipe(replace('url(%%spritesheet%%)', 'url("https://localhost:4443/images/spritesheet.png")'))
+        .pipe(gulpif((argv.production !== true), replace('url(%%body%%)', 'url("https://localhost:4443/images/body.png")')))
+        .pipe(gulpif((argv.production !== true), replace('url(%%overlay%%)', 'url("https://localhost:4443/images/overlay.png")')))
+        .pipe(gulpif((argv.production !== true), replace('url(%%header%%)', 'url("https://localhost:4443/images/header.png")')))
+        .pipe(gulpif((argv.production !== true), replace('url(%%crosshatch%%)', 'url("https://localhost:4443/images/crosshatch.png")')))
+        .pipe(gulpif((argv.production !== true), replace('url(%%editbuttons%%)', 'url("https://localhost:4443/images/editbuttons.png")')))
+        .pipe(gulpif((argv.production !== true), replace('url(%%spritesheet%%)', 'url("https://localhost:4443/images/spritesheet.png")')))
         .pipe(cleanCSS({debug: true}))
         .pipe(gulp.dest('build/'));
 
