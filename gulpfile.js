47cb9c7383a3010205e98f82bfbe3f989cfe800d
ncla <ncla@users.noreply.github.com>
2017-04-23 17:23:42 +0300

    Hype thread styling

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ gulp.task('build', function() {
         .pipe(gulpif((argv.production !== true), replace('url(%%editbuttons%%)', 'url("https://localhost:4443/images/editbuttons.png")')))
         .pipe(gulpif((argv.production !== true), replace('url(%%spritesheet%%)', 'url("https://localhost:4443/images/spritesheet.png")')))
         .pipe(gulpif((argv.production !== true), replace('url(%%flairsheet%%)', 'url("https://localhost:4443/images/flairsheet.png")')))
+        .pipe(gulpif((argv.production !== true), replace('url(%%header_hype%%)', 'url("https://localhost:4443/images/header_hype.jpg")')))
         .pipe(concatCss('stylesheet.css'))
         .pipe(cleanCSS({
             debug: true,
